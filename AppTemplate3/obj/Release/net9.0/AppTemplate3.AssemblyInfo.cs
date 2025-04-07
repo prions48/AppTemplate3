e8fdce7fb1737fc802bab8bd7d40e87ebaf96376
Alejandro Enriquez <prions48@gmail.com>
2025-04-06 19:04:12 -0500

    some minor debugging on file handling

--- AppTemplate3/obj/Release/net9.0/AppTemplate3.AssemblyInfo.cs
+++ AppTemplate3/obj/Release/net9.0/AppTemplate3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466874c75acf58ab3c5f6dac14c19dfe17e77f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9241de8598c3dac943d8598dfc09fd1ea71cdcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
