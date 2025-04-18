ff847238145bf4323c236e1655ef8db0fbae3f27
Alejandro Enriquez <prions48@gmail.com>
2025-04-17 19:06:41 -0500

    extra minor debugging

--- AppTemplate3/obj/Debug/net9.0/AppTemplate3.AssemblyInfo.cs
+++ AppTemplate3/obj/Debug/net9.0/AppTemplate3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9241de8598c3dac943d8598dfc09fd1ea71cdcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8fdce7fb1737fc802bab8bd7d40e87ebaf96376")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTemplate3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
